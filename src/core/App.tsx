63e8e164d17883c5d8e4bba0674805253f2cbb06
Malvare22 <pingoalex22@gmail.com>
2024-10-04 15:19:39 -0500

    feat: :art: Add fonts in .css

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,12 +1,15 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./router/router"
+import './fonts/fonts.css'
+import { ThemeProvider } from "@emotion/react"
+import { theme } from "./themes"
 
 function App() {
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <RouterProvider router={router}/>
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/modules/general/pages/index/index.tsx
+++ src/modules/general/pages/index/index.tsx
@@ -4,9 +4,13 @@ export default function Test() {
   return (
     <Box sx={{
       border: 'black 1px solid',
+      width: '100%',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center'
 
     }}>
-      
+        Hola mundo
     </Box>
   )
 }
