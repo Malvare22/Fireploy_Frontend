714b0856169da079c2ecc7bc5231f2c8228f5551
Rodrigo Malaver <pingoalex22@hotmail.com>
2024-10-12 22:57:48 -0400

    test(module): intent alias paths

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./themes";
 // import '@/core/fonts/Open Sans/fonts.css'
-import "@/core/fonts/OpenSans/fonts.css";
+import "@src/core/fonts/OpenSans/fonts.css";
 import { router } from "./router/router";
 import { SnackBarContext } from "../modules/general/context/snackbarContext";
 import useSnackBar from "../modules/general/hooks/useSnackbar";
 import SnackBar from "../modules/general/components/snackbar";
+
+
 function App() {
   const {view, setView, message, setMessage, success, setSuccess} = useSnackBar();
 
