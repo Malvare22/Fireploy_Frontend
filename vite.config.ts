c824120934df85b22d82bee61c88c58b0dc7aa6b
Rodrigo Andrés Malaver Suárez <pingoalex22@hotmail.com>
2024-10-13 19:27:17 -0500

    fix(modules): :zap: add config to absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'src/*': 'src/*'
+      "@core" : "/src/core",
+      "@modules" : "/src/modules",
     },
   },
 });
