9856773b98659766044087d2f308437d1cc3263e
Pingoalex22 <pingoalex22@hotmail.com>
2024-12-27 15:27:16 -0500

    feat(navbar): add admin navigation system

--- src/modules/general/components/navbar/components/drawer/index.tsx
+++ src/modules/general/components/navbar/components/drawer/index.tsx
@@ -1,17 +1,72 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import { IconButton } from '@mui/material';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { IconButton, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import DocumentScannerIcon from "@mui/icons-material/DocumentScanner";
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
+import NoteAltIcon from "@mui/icons-material/NoteAlt";
+import CloudCircleIcon from "@mui/icons-material/CloudCircle";
+import ExploreIcon from "@mui/icons-material/Explore";
+import { useNavigate } from "react-router-dom";
 
+type PairIcons = {
+  icon: React.ReactNode;
+  label: string;
+  url: string;
+};
+
+const myProjects: PairIcons = {
+  icon: <AccountTreeIcon />,
+  label: "Mis Proyectos",
+  url: "myProjects",
+};
+
+const myPortafolio: PairIcons = {
+  icon: <DocumentScannerIcon />,
+  label: "Mi Portafolio",
+  url: "myPortafolio",
+};
+
+const users: PairIcons = {
+  icon: <SupervisedUserCircleIcon />,
+  label: "Usuarios",
+  url: "users",
+};
+
+const subjects: PairIcons = {
+  icon: <NoteAltIcon />,
+  label: "Materias y Cursos",
+  url: "subjects",
+};
+
+const projects: PairIcons = {
+  icon: <CloudCircleIcon />,
+  label: "Proyectos",
+  url: "projects",
+};
+
+const safari: PairIcons = {
+  icon: <ExploreIcon />,
+  label: "Explorar Portafolios",
+  url: "portafolios",
+};
+
+const admin: PairIcons[] = [
+  myProjects,
+  myPortafolio,
+  users,
+  subjects,
+  projects,
+  safari,
+];
 
 export default function DrawerCustom() {
   const [open, setOpen] = React.useState(false);
@@ -20,41 +75,44 @@ export default function DrawerCustom() {
     setOpen(newOpen);
   };
 
+  const navigate = useNavigate();
+
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+    <Box
+      sx={{ width: { xs: 240, md: 360 } }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+    >
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+        {admin.map((option, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton onClick={() => navigate(option.url)}>
+              <ListItemIcon
+                sx={{
+                  "& svg": {
+                    fontSize: 32,
+                  },
+                }}
+              >
+                {option.icon}
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText
+                primary={<Typography variant="h6">{option.label}</Typography>}
+              />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
       <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <List></List>
     </Box>
   );
 
   return (
     <div>
-        <IconButton onClick={toggleDrawer(true)}>
-            <MenuIcon sx={{fontSize: 32, color: 'white'}}></MenuIcon>
-          </IconButton>
+      <IconButton onClick={toggleDrawer(true)}>
+        <MenuIcon sx={{ fontSize: 32, color: "white" }}></MenuIcon>
+      </IconButton>
       <Drawer open={open} onClose={toggleDrawer(false)}>
         {DrawerList}
       </Drawer>
