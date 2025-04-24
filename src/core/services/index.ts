f6907d17e33ef5aae89e7d91ea5283747f4ec2cd
Rodrigo Andrés Malaver Suárez <pingoalex22@hotmail.com>
2025-04-23 21:13:39 -0500

    fix: query login

--- src/core/services/index.ts
+++ src/core/services/index.ts
@@ -26,7 +26,7 @@ export interface ApiResponse<T> {
  */
 const apiClient: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_URL_BACKEND as string,
-  timeout: 1000,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
