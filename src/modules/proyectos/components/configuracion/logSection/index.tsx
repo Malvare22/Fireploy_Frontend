7a8b025934e17f7257e93f2eedfef7593514354f
Rodrigo Andrés Malaver Suárez <pingoalex22@hotmail.com>
2025-05-24 12:22:38 -0500

    fix: logs section
    sort logs, refactor query data and useState, split logs by type of repository

--- src/modules/proyectos/components/configuracion/logSection/index.tsx
+++ src/modules/proyectos/components/configuracion/logSection/index.tsx
@@ -2,27 +2,30 @@ import {
   Accordion,
   Alert,
   Box,
+  capitalize,
   Divider,
   Pagination,
   Stack,
+  Tab,
+  Tabs,
   Typography,
+  useMediaQuery,
   useTheme,
 } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { LogFile } from "@modules/proyectos/types/logFile";
 import { useQuery } from "@tanstack/react-query";
 import { getRepositoryById } from "@modules/proyectos/services/get.repository";
-import { KeysOfRepository } from "@modules/proyectos/types/keysOfRepository";
+import { getKeyOfRepository, KeysOfRepository } from "@modules/proyectos/types/keysOfRepository";
 import { useAuth } from "@modules/general/context/accountContext";
 import { adaptLog } from "@modules/proyectos/utils/adaptLog";
-import { getRepositoryTypesMap } from "@modules/proyectos/utils/repository";
-import { TipoRepositorio } from "@modules/proyectos/types/proyecto.tipo";
 import { useAlertDialogContext } from "@modules/general/context/alertDialogContext";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import useErrorReader from "@modules/general/hooks/useErrorReader";
 import { usePagination } from "@modules/general/hooks/usePagination";
+import { getFormatDayTime } from "@modules/general/utils/fechas";
 
 export enum labelLogs {
   titulo = "Logs & Mensajes",
@@ -36,56 +39,50 @@ export enum labelLogs {
 type LogsFilesProps = Record<KeysOfRepository, number | undefined>;
 function LogsFiles({ backend, frontend, integrado }: LogsFilesProps) {
   const { token } = useAuth().accountInformation;
+
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const [logsByField, setLogsByField] = useState<Map<KeysOfRepository, LogFile[]>>();
+
   const { data, error } = useQuery({
     queryFn: async () => {
-      let response: LogFile[] = [];
-      if (backend) {
-        const repo = await getRepositoryById(token, backend);
-        if (repo.logs) {
-          repo.logs.forEach((x) => {
-            response.push(
-              adaptLog(
-                x,
-                getRepositoryTypesMap.get(repo.tipo as TipoRepositorio) as KeysOfRepository
-              )
-            );
-          });
+      let response = new Map<KeysOfRepository, LogFile[]>();
+
+      const setLogOfRepo = async (id: number | undefined) => {
+        if (id) {
+          const repo = await getRepositoryById(token, id);
+          const key = getKeyOfRepository(repo.tipo);
+          const tmp: LogFile[] = [];
+          if (repo.logs) {
+            repo.logs.forEach((x) => tmp.push(adaptLog(x, key)));
+          }
+          response.set(key, tmp);
         }
-      }
-      if (frontend) {
-        const repo = await getRepositoryById(token, frontend);
-        if (repo.logs) {
-          repo.logs.forEach((x) => {
-            response.push(
-              adaptLog(
-                x,
-                getRepositoryTypesMap.get(repo.tipo as TipoRepositorio) as KeysOfRepository
-              )
-            );
-          });
-        }
-      }
-      if (integrado) {
-        const repo = await getRepositoryById(token, integrado);
-        if (repo.logs) {
-          repo.logs.forEach((x) => {
-            response.push(
-              adaptLog(
-                x,
-                getRepositoryTypesMap.get(repo.tipo as TipoRepositorio) as KeysOfRepository
-              )
-            );
-          });
-        }
-      }
+      };
+
+      await setLogOfRepo(backend);
+      await setLogOfRepo(frontend);
+      await setLogOfRepo(integrado);
+
       return response;
     },
     queryKey: ["A", token],
   });
 
+  useEffect(() => {
+    if (data) {
+      setLogsByField(data);
+      if (logsByField?.get("backend")) setTabIndex(1);
+    }
+  }, [data]);
+
   const { showDialog } = useAlertDialogContext();
 
   const { setError } = useErrorReader(showDialog);
+
+  const theme = useTheme();
+
+  const matchesMedia = useMediaQuery(theme.breakpoints.down("sm"));
 
   useEffect(() => {
     if (error) setError(error);
@@ -100,29 +97,64 @@ function LogsFiles({ backend, frontend, integrado }: LogsFilesProps) {
       <Box>
         <Typography variant="body1">{labelLogs.parrafo}</Typography>
       </Box>
-      {data && data.length > 0 ? (
-        <Container logsFiles={data} />
-      ) : (
-        <Alert severity="info">{"No dispones de registros .log actualmente"}</Alert>
-      )}
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        {backend && frontend && (
+          <Tabs
+            value={tabIndex}
+            onChange={(_e, newIndex) => setTabIndex(newIndex)}
+            sx={{
+              borderBottom: 1,
+              borderColor: "divider",
+              "& .MuiButtonBase-root": {
+                paddingY: 0,
+              },
+            }}
+            variant="scrollable"
+            scrollButtons={matchesMedia}
+            allowScrollButtonsMobile
+          >
+            <Tab label="Backend" value={1} />
+            <Tab label="Frontend" value={2} />
+          </Tabs>
+        )}
+        {tabIndex == 0 && <Container logsFiles={logsByField?.get("integrado")} />}
+        {tabIndex == 1 && <Container logsFiles={logsByField?.get("backend")} />}
+        {tabIndex == 2 && <Container logsFiles={logsByField?.get("frontend")} />}
+      </Box>
     </Box>
   );
 }
 
-type ContainerProps = { logsFiles: LogFile[] };
+type ContainerProps = { logsFiles: LogFile[] | undefined };
 const Container = ({ logsFiles }: ContainerProps) => {
+  const currentLogs = logsFiles || [];
+
+  function sortLogs(a: LogFile, b: LogFile) {
+    const dateA = new Date(a.fecha).getTime();
+    const dateB = new Date(b.fecha).getTime();
+    return dateB - dateA;
+  }
+
   const { goToPage, hasNextPage, hasPrevPage, totalPages, paginatedData } = usePagination(
-    logsFiles,
-    5,
+    currentLogs.sort(sortLogs),
+    10,
     1
   );
 
   return (
     <>
       <Stack>
-        {paginatedData.map((log, index) => (
-          <CardLog key={index} currentLog={log} />
-        ))}
+        {logsFiles == undefined || logsFiles.length == 0 ? (
+          <Alert severity="info">
+            {"No dispones de registros .log para este repositorios actualmente"}
+          </Alert>
+        ) : (
+          <>
+            {paginatedData.map((log, index) => (
+              <CardLog key={index} currentLog={log} />
+            ))}
+          </>
+        )}
       </Stack>
       <Box
         sx={{
@@ -158,17 +190,17 @@ const CardLog: React.FC<{
           id="panel2-header"
         >
           <Stack direction={"column"} spacing={2}>
-            <Box sx={{ display: "flex" }}>
+            <Box sx={{ display: "flex", gap: 1 }}>
               <Typography variant="body1" sx={{ fontWeight: 500 }}>
                 {labelLogs.instancia}
               </Typography>
               <Typography variant="body1" sx={{ fontWeight: 500 }}>
-                {currentLog.instancia}
+                {capitalize(currentLog.instancia)}
               </Typography>
             </Box>
-            <Box sx={{ display: "flex" }}>
+            <Box sx={{ display: "flex", gap: 1 }}>
               <Typography variant="body2">{labelLogs.fecha}</Typography>
-              <Typography variant="body2">{currentLog.fecha}</Typography>
+              <Typography variant="body2">{getFormatDayTime(currentLog.fecha)}</Typography>
             </Box>
           </Stack>
         </AccordionSummary>
