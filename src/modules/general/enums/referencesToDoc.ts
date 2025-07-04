a07f450a40a725ce8fcb7f7ad5bdec72f010504f
FireployOfficial <fireploy.official@gmail.com>
2025-07-03 22:17:37 -0500

    feat: create doc_reference enum and change some projects aspects
    Not show alert for deploy to set changes in create and fix alert for special envs

---
+++ src/modules/general/enums/referencesToDoc.ts
@@ -0,0 +1,3 @@
+export enum REFERENCE_TO_DOC {
+    SPECIAL_VARIABLES = 'https://app103.proyectos.fireploy.online/docs/estudiante/usuario-general/proyectos/proceso-de-despliegue/variables-especiales/'
+}
