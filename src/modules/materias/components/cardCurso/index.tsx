f23439dd2e2baf5f8ff54bb4f133b4f3ded3d090
Rodrigo Andrés Malaver Suárez <72281004+Malvare22@users.noreply.github.com>
2025-08-27 22:14:52 -0500

    Rework (#57)

--- src/modules/materias/components/cardCurso/index.tsx
+++ src/modules/materias/components/cardCurso/index.tsx
@@ -19,6 +19,7 @@ import { rutasUsuarios } from "@modules/usuarios/router/routes";
 import { rutasMaterias } from "@modules/materias/router/routes";
 import SchoolIcon from "@mui/icons-material/School";
 import EditNoteIcon from "@mui/icons-material/EditNote";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 type CardCursoProps = {
   curso: Curso;
@@ -40,7 +41,7 @@ const CardCurso: React.FC<CardCursoProps> = ({
   const navigate = useNavigate();
 
   const { id } = useAuth().accountInformation;
-  
+
   const theme = useTheme();
 
   const showDocentOwnerStyle = !(userType != "D" || (userType == "D" && !isRegister));
@@ -49,7 +50,11 @@ const CardCurso: React.FC<CardCursoProps> = ({
    * Un docente no puede solicitar un curso ya ocupado por otro docente
    */
   const buttonDisable = () => {
-    return hasActiveRequest || (userType == "D" && curso.docente != null && curso.docente.id != id);
+    return (
+      hasActiveRequest ||
+      (userType == "D" && curso.docente != null && curso.docente.id != id) ||
+      curso.estado == "I"
+    );
   };
 
   const buttonElements = useMemo((): { color: "primary" | "warning"; text: string } => {
@@ -61,7 +66,11 @@ const CardCurso: React.FC<CardCursoProps> = ({
       if (curso.docente != null && curso.docente.id != id)
         return { text: "Curso no disponible", color: "warning" };
     }
+
     if (userType == "E" && !isRegister) {
+      if (curso.estado == "I") {
+        return { text: "Curso no disponible", color: "warning" };
+      }
       return { text: "Inscribirme", color: "warning" };
     }
 
@@ -117,6 +126,7 @@ const CardCurso: React.FC<CardCursoProps> = ({
             <ActivityAndStudents
               cntStudents={(curso.estudiantes ?? []).length}
               cntActities={(curso.secciones ?? []).length}
+              semester={curso.semestre}
             />
             {!showDocentOwnerStyle && (
               <FrameDocente docente={!curso.docente ? null : (curso.docente as UsuarioCurso)} />
@@ -193,32 +203,17 @@ function FrameDocente({ docente }: PropsFrameDocente) {
     );
 }
 
-/**
- * ActivityAndStudents component – shows a summary of the course's activities and enrolled students.
- *
- * It visually represents counts using Material UI chips with icons.
- *
- * @component
- *
- * @param {number} cntActities - Number of activities in the course.
- * @param {number} cntStudents - Number of students enrolled in the course.
- *
- * @returns {JSX.Element} A horizontal stack with labeled chips for activities and students.
- *
- * @example
- * ```tsx
- * <ActivityAndStudents cntActities={5} cntStudents={30} />
- * ```
- */
 export function ActivityAndStudents({
   cntActities,
   cntStudents,
+  semester,
 }: {
   cntStudents: number;
   cntActities: number;
+  semester: string;
 }) {
   return (
-    <Stack direction={"row"} spacing={2}>
+    <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: {xs: "center", md: 'left'}, gap: 1 }}>
       <Chip
         label={`Actividades: ${cntActities}`}
         icon={<EditNoteIcon />}
@@ -231,6 +226,12 @@ export function ActivityAndStudents({
         size={"medium"}
         icon={<SchoolIcon />}
       />
-    </Stack>
+      <Chip
+        label={`Semestre: ${semester}`}
+        color="error"
+        size={"medium"}
+        icon={<CalendarMonthIcon />}
+      />
+    </Box>
   );
 }
